5d0dd60ae0a8f9cb2f48c79dc6aa8d2b51ee69f1
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-15 21:32:37 -0300

    mooc-java-programming-17

--- mooc-java-programming-i/part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ mooc-java-programming-i/part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -6,7 +6,13 @@ public class SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        Integer first = Integer.valueOf(scanner.nextLine());
 
+        System.out.println("Give the second number:");
+        Integer second = Integer.valueOf(scanner.nextLine());
+
+        Integer total = first + second;
+        System.out.println("The sum of the numbers is " + total);
     }
 }
