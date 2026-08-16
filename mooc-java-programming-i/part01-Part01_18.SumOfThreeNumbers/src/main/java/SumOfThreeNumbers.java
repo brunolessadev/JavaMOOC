584612dfc9e905fb42676c256ab562cbb99e3cc9
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-16 14:43:12 -0300

    mooc-java-programming-18

--- mooc-java-programming-i/part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ mooc-java-programming-i/part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -6,7 +6,17 @@ public class SumOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        Integer first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        Integer second = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        Integer third = Integer.valueOf(scanner.nextLine());
+
+        Integer total = first + second + third;
+        System.out.println("The sum of the numbers is " + total);
 
     }
 }
