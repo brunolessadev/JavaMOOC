e480c6999607f1b98630ebbab01f0ed2e85aaa89
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-18 20:47:18 -0300

    mooc-java-programming-23

--- mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -6,7 +6,21 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        Integer first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        Integer second = Integer.valueOf(scanner.nextLine());
+
+        int total1 = first + second;
+        int total2 = first - second;
+        int total3 = first * second;
+        Double total4 = (double)first / second;
+
+        System.out.println(first + " + " + second + " = " + total1);
+        System.out.println(first + " - " + second + " = " + total2);
+        System.out.println(first + " * " + second + " = " + total3);
+        System.out.println(first + " / " + second + " = " + total4);
 
     }
 }
