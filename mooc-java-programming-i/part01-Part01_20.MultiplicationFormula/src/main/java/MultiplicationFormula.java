11ee11e7c8486f20bd417fa3afb4723b169ec1be
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-18 19:29:07 -0300

    mooc-java-programming-20

--- mooc-java-programming-i/part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ mooc-java-programming-i/part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -6,7 +6,14 @@ public class MultiplicationFormula {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        Integer first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        Integer second = Integer.valueOf(scanner.nextLine());
+
+        Integer total = first * second;
+        System.out.println( first + " * " + second + " = " + total);
 
     }
 }
