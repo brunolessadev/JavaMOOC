1922b6dd247f742f08d00092a2344cecb10f4f34
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-23 17:46:45 -0300

    mooc-java-programming-35

--- mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -6,5 +6,13 @@ public class CheckingTheAge {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("How old are you?");
+        Integer year = Integer.valueOf(scan.nextLine());
+
+        if(year < 0 || year > 120){
+            System.out.println("Impossible!");
+        }else{
+            System.out.println("OK");
+        }
     }
 }
