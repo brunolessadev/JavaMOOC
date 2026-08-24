3736cfb89348556485c26a54c5f0d607ce339a1e
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 09:02:23 -0300

    mooc-java-programming-p02-08

--- mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int ones = 0;
 
+        while (true) {
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+            ones = ones + 1;
+
+        }
+        System.out.println("Number of numbers: " + ones);
     }
 }
