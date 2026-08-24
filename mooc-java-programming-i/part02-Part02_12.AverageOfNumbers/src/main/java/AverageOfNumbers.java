f2232b828e27afa957187ce6906627f0e847a3cd
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 09:38:19 -0300

    mooc-java-programming-p02-12

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,10 +1,28 @@
 
+
 import java.util.Scanner;
 
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Integer one = 0;
+        Integer two = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+
+            if(number == 0){
+                break;
+            }else{
+                one = one + 1;
+                two = two + number;
+            }
+        }
+        Double three = (double) two / one;
+        System.out.println("Average of the numbers: " + three);
 
     }
 }
