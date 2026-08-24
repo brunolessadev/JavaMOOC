327ad04ec0b6d13c35c7fe07ac49122282da31fd
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 09:24:23 -0300

    mooc-java-programming-p02-10

--- mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,18 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Integer one = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+            if(number == 0){
+                break;
+            }else {
+                one = (one + number);
+            }
+        }
+        System.out.println("Sum of the numbers: " + one);
 
     }
 }
