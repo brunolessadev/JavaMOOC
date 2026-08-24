b01f6f9b29caa9ab83040ef35d781d92da750a66
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 08:53:15 -0300

    mooc-java-programming-p02-07

--- mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,18 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.println("Give a number:");
+            Integer number = Integer.valueOf(scanner.nextLine());
+            if(number < 0){
+                System.out.println("Unsuitable number");
+            } else if (number > 0) {
+                number = number * number;
+                System.out.println(number);
+            }else {
+                break;
+            }
+        }
+
     }
 }
