7852a064a68626affd87ec2db18be15b89c5e8d7
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 16:01:03 -0300

    mooc-java-programming-p02-33

--- mooc-java-programming-i/part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ mooc-java-programming-i/part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -1,3 +1,4 @@
+import org.w3c.dom.ls.LSOutput;
 
 public class StarSign {
 
@@ -19,18 +20,27 @@ public class StarSign {
     }
 
     public static void printStars(int number) {
-        // first part of the exercise
+        for (int i = 0; i < number ; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSquare(int size) {
-        // second part of the exercise
+        for (int i = 0; i < size ; i++) {
+            printStars(size);
+        }
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+        for (int i = 0; i < height ; i++) {
+            printStars(width);
+        }
     }
 
     public static void printTriangle(int size) {
-        // fourth part of the exercise
+        for (int i = 1; i <= size ; i++) {
+            printStars(i);
+        }
     }
 }
