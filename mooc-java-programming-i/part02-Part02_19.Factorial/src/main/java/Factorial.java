5951df7b82f0546bcd3a0a7fe912f3edbe909b5b
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 12:17:34 -0300

    mooc-java-programming-p02-19

--- mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,15 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int one = 1;
+
+        System.out.println("Give a number");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i  <= number; i++){
+            one = one * i;
+        }
+
+        System.out.println("Factorial: " + one);
     }
 }
