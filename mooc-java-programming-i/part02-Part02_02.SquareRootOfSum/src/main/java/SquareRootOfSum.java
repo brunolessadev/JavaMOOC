f80c12097744c3c11dd893a9ff80d21a6e9d1094
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 08:18:56 -0300

    mooc-java-programming-p02-02

--- mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,11 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Integer number1 = Integer.valueOf(scanner.nextLine());
+        Integer number2 = Integer.valueOf(scanner.nextLine());
+
+        Double squareRoot = Math.sqrt(number1 + number2);
+        System.out.println(squareRoot);
 
     }
 }
