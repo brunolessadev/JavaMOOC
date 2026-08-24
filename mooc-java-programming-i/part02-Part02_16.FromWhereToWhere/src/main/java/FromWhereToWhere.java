1c91dde35ff7b9c2e85aa6cdef9ce41742c7baf1
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 10:19:29 -0300

    mooc-java-programming-p02-16

--- mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,18 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+
+        System.out.println("Where to?");
+        Integer whereTo = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Where from?");
+        Integer whereFrom = Integer.valueOf(scanner.nextLine());
+
+
+
+
+        for( int i = whereFrom; i < whereTo + 1 ; i ++){
+            System.out.println(i);
+        }
     }
 }
