1d24909083815f79bd2f8b274e6f56e1fbfc133a
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 13:21:16 -0300

    mooc-java-programming-p02-23

--- mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        printUntilNumber(number);
     }
 
+    public static void printUntilNumber(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i);
+        }
+    }
 }
