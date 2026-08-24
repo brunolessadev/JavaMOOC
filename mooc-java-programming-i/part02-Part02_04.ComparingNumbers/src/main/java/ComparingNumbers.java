3ed72d6e118a04949f0c61dc5faa680c59bc5875
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 08:34:02 -0300

    mooc-java-programming-p02-04

--- mooc-java-programming-i/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ mooc-java-programming-i/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -6,5 +6,16 @@ public class ComparingNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Integer number1 = Integer.valueOf(scanner.nextLine());
+        Integer number2 = Integer.valueOf(scanner.nextLine());
+
+        if (number1 > number2){
+            System.out.println(number1 + " is greater than " + number2 + ".");
+        } else if (number1 < number2) {
+            System.out.println(number1 + " is smaller than " + number2 + ".");
+        }else {
+            System.out.println(number1 + " is equal to " + number2 + ".");
+        }
+
     }
 }
