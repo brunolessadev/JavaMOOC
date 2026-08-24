268d57215d12233eb38ad36b5ce1bde527841901
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 15:18:01 -0300

    mooc-java-programming-p02-28

--- mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
+++ mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
@@ -6,6 +6,10 @@ public class Word {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String value = word();
+    }
+    public static String word(){
+        return "";
     }
 
 }
