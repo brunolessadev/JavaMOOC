13b6c295ea5f52e5889ed5aefbf5be7290a2cbc0
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 10:25:52 -0300

    mooc-java-programming-p02-17

--- mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,15 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int i = 0;
+        int total = 0;
+
+        Integer n = Integer.valueOf(scanner.nextLine());
+
+        while(i < n){
+            i++;
+            total = total + i;
+        }
+        System.out.println(total);
     }
 }
