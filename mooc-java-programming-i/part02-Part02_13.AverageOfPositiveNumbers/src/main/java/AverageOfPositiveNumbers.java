d83e4f6520307c9b80bb2d313ffdb32bffb137a8
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-24 09:48:18 -0300

    mooc-java-programming-p02-13

--- mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,27 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int one = 0;
+        int two = 0;
+        int number = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            number = Integer.parseInt(scanner.nextLine());
+
+            if( number == 0){
+                break;
+            }else if(number > 0){
+                one = one + 1;
+                two = two + number;
+            }
+        }
+
+        if(one > 0){
+            double three = ((double) two /one);
+            System.out.println(three);
+        }else {
+            System.out.println("Cannot calculate the average");
+        }
     }
 }
