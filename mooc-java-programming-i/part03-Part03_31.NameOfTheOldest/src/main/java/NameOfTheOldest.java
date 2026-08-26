15dc655b829b84753cb79260e12779e6a59adc98
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-26 01:12:13 -0300

    mooc-java-programming-p03-31

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -7,5 +7,24 @@ public class NameOfTheOldest {
         Scanner scanner = new Scanner(System.in);
 
 
+        int oldest = 0;
+        String name = "";
+
+        while (true){
+            String text = scanner.nextLine();
+
+            if(text.equals("")){
+                break;
+            }
+
+            String[] pieces = text.split(",");
+            int age = Integer.valueOf(pieces[1]);
+
+            if(age > oldest){
+                oldest = age;
+                name = pieces[0];
+            }
+        }
+        System.out.println("Name of the oldest: " + name);
     }
 }
