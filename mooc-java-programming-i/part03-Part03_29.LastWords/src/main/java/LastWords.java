619a022006caa2d906b8228546fc418e1dc780e8
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-26 00:57:06 -0300

    mooc-java-programming-p03-29

--- mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,18 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String text = String.valueOf(scanner.nextLine());
+            if (text.equals("") || text.equals(" ")) {
+                break;
+            }
 
+            String[] pieces = text.split(" ");
+
+            int index = pieces.length;
+            System.out.println(pieces[index-1]);
+
+            }
+        }
     }
-}
+
