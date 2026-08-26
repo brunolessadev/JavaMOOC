d8df527b405e02d3b1c58cdb4991eb39fee2ba4f
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-26 01:02:54 -0300

    mooc-java-programming-p03-30

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,21 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int oldest = 0;
 
+        while (true){
+            String text = String.valueOf(scanner.nextLine());
+            if(text.equals("") || text.equals(" ")){
+                break;
+            }
+
+            String[] pieces = text.split(",");
+            int age = Integer.valueOf(pieces[1]);
+
+            if(age > oldest){
+                oldest = age;
+            }
+        }
+        System.out.println("Age of the oldest: " + oldest);
     }
 }
