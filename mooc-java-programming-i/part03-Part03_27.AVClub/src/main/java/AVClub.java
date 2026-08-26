3a9ec5a3766bc0e1827387415784c209b87d8dbd
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-26 00:37:40 -0300

    mooc-java-programming-p03-27

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,18 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true){
+            String text = String.valueOf(scanner.nextLine());
+            if(text.equals("") || text.equals(" ")){
+                break;
+            }
+            String[] pieces = text.split(" ");
+            for (int i = 0; i < pieces.length; i++) {
+                if (pieces[i].contains("av")){
+                    System.out.println(pieces[i]);
+                }
+            }
+        }
 
     }
 }
