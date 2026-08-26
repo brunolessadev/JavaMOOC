f3732ffbf6e34d6399fd5140c2ee09c4b02ffb72
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-26 01:24:32 -0300

    mooc-java-programming-p03-32

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -8,5 +8,31 @@ public class PersonalDetails {
         Scanner scanner = new Scanner(System.in);
 
 
+        String longestName = "";
+        int sumOfBirthYears = 0;
+        int count = 0;
+
+        while (true) {
+            String text = scanner.nextLine();
+
+            if (text.equals("")) {
+                break;
+            }
+
+            String[] pieces = text.split(",");
+            String name = pieces[0];
+            int birthYear = Integer.parseInt(pieces[1]);
+
+            if (name.length() > longestName.length()){
+                longestName = name;
+            }
+            sumOfBirthYears = sumOfBirthYears + birthYear;
+            count++;
+        }
+        System.out.println("Longest name: " + longestName);
+        if (count > 0) {
+            System.out.println("Average of the birth years: " + ((double) sumOfBirthYears / count));
+        }
     }
 }
+
