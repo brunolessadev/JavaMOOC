8b0d8e9df8f9c4e1b266594515b861e0a3be482f
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-26 00:23:29 -0300

    mooc-java-programming-p03-25

--- mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -6,5 +6,16 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter username:");
+        String userName = String.valueOf(scanner.nextLine());
+        System.out.println("Enter password:");
+        String password = String.valueOf(scanner.nextLine());
+
+        if(userName.equals("alex") && password.equals("sunshine")
+                || userName.equals("emma") && password.equals(("haskell"))){
+            System.out.println("You have successfully logged in!");
+        }else{
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
