81057e7eab1a3ecd23d85f0c4f2f3615c41a56f4
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-27 11:52:42 -0300

    mooc-java-programming-p04-22

--- mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,17 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int cube = 0;
 
+        while (true){
+            String input = scanner.nextLine();
+
+            if(input.equals("end")){
+                break;
+            }
+            cube = Integer.parseInt(input);
+            cube = cube * cube * cube;
+            System.out.println(cube);
+        }
     }
 }
