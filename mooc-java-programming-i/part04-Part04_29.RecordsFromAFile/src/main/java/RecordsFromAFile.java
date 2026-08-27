807dd973888f94a06f94402d086d216013903f97
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-27 16:14:59 -0300

    mooc-java-programming-p04-29

--- mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -7,5 +7,25 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the files:");
+        String file = scanner.nextLine();
+
+        try (Scanner fileScanner = new Scanner(Paths.get(file))){
+            while (fileScanner.hasNextLine()){
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(",");
+
+                String name = parts[0];
+                Integer age = Integer.valueOf(parts[1]);
+
+                if (age == 1) {
+                    System.out.println(name + ", age: " + age + " year");
+                } else {
+                    System.out.println(name + ", age: " + age + " years");
+                }
+            }
+        }catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
