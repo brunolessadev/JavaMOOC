881bc5192d2db75c181d6fed197bff41105ee771
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-09-04 15:31:13 -0300

    mooc-java-programming-p05-09

--- mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,23 @@
 
 public class HealthStation {
 
+    private int weighingsCount;
+    public HealthStation(){
+        this.weighingsCount = 0;
+    }
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        this.weighingsCount++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person){
+        int pesoAtual = person.getWeight();
+        person.setWeight(pesoAtual + 1);
+    }
+
+    public int weighings(){
+        return this.weighingsCount;
     }
 
 }
