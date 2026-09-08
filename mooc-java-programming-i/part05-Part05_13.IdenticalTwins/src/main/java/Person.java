3eae122b0edfa3491a07cc9e2ba90dff4320b9f6
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-09-08 11:26:33 -0300

    mooc-java-programming-p05-13

--- mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -1,5 +1,4 @@
 
-import java.util.Objects;
 
 public class Person {
 
@@ -15,5 +14,13 @@ public class Person {
         this.weight = weight;
     }
 
-    // implement an equals method here for checking the equality of objects
+    public boolean equals (Object compared) {
+        if( this  == compared)return true;
+        if(!(compared instanceof Person)) return false;
+        Person comparedPerson = (Person) compared;
+        return this.name.equals (comparedPerson.name)
+                && this.birthday.equals(comparedPerson.birthday)
+                && this.height == (comparedPerson.height)
+                && this.weight == (comparedPerson.weight) ;
+    }
 }
