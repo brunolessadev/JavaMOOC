e0ef3fe567544139584ef04a5dcb17ee8f9c249e
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-09-21 14:13:13 -0300

    mooc-java-programming-p07-01

--- mooc-java-programming-i/part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ mooc-java-programming-i/part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -6,16 +6,56 @@ public class LiquidContainers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int first = 0;
+        int second = 0;
+
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
 
+            System.out.print("> ");
             String input = scan.nextLine();
+
             if (input.equals("quit")) {
                 break;
             }
 
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.parseInt(parts[1]);
+
+
+            if(command.equals("add") && amount > 0){
+                first = first + amount;
+                if(first > 100){
+                    first = 100;
+                }
+            }
+
+            if(command.equals("move") && amount > 0){
+
+                if (amount > first ){
+                    amount = first;
+                }
+
+                first = first - amount;
+                second = second + amount;
+
+
+                if(second > 100){
+                    second = 100;
+                }
+            }
+
+            if (command.equals("remove") && amount > 0){
+
+                second = second - amount;
+
+                if (second < 0){
+                    second = 0;
+                }
+            }
         }
     }
-
 }
